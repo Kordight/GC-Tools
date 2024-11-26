30ea67d459b88795fd6d743de678be6922185da1
Kordight <164764949+Kordight@users.noreply.github.com>
2024-11-26 23:02:40 +0100

    Update index.php

--- text_replacer/index.php
+++ text_replacer/index.php
@@ -23,7 +23,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Usuwanie Tekstu</title>
     <link rel="stylesheet" href="../style.css">
-    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
 </head>
 
 <body>
