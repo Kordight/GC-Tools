2dfbef7b5cfaaccee2ae8e458aea57eefd8e1be5
Kordight <164764949+Kordight@users.noreply.github.com>
2024-11-26 23:01:22 +0100

    ico

--- text_replacer/index.php
+++ text_replacer/index.php
@@ -23,6 +23,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Usuwanie Tekstu</title>
     <link rel="stylesheet" href="../style.css">
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 </head>
 
 <body>
